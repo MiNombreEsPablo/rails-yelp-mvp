883d0538643024e7f9d8376242dae6fcccc95126
Pablo Bugueno <pablo.buguenoe@gmail.com>
2023-07-04 20:02:17 +0900

    rspec passes

--- app/models/review.rb
+++ app/models/review.rb
@@ -8,5 +8,5 @@ class Review < ApplicationRecord
   validates :content, :rating, presence: true
   # A review’s rating must be a number between 0 and 5.
   # A review’s rating must be an integer. For example, a review with a rating of 2.5 should be invalid!
-  validates :rating, inclusion: { in: (1..5) }
+  validates :rating, inclusion: { in: (0..5) }, numericality: { only_integer: true }
 end
