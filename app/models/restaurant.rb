cc31b710f4b6880d318d811c8d77d318d88e7a9d
Pablo Bugueno <pablo.buguenoe@gmail.com>
2023-07-04 19:52:04 +0900

    added review model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class Restaurant < ApplicationRecord
+  # A restaurant must have a name, an address and a category.
+  validates :name, :address, :category, presence: true
+  # A restaurant’s category must belong to this fixed list: ["chinese", "italian", "japanese", "french", "belgian"].
+  validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }
+  # When a restaurant is destroyed, all of its reviews must be destroyed as well.
+  # has_many :reviews, dependent: :destroy
 end
